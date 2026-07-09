f1ce2b9f81e7eabaff077f64f2f9e5a2b11257b2
Tryone-debug <2664710900@qq.com>
2026-07-09 10:06:32 +0800

    上传ROS工作空间catkin_ws

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/p/catkin_ws/src
+# Build directory: /home/p/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("learning_service")
+subdirs("learning_topic")
---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/p/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/p/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/p/catkin_ws/devel;/opt/ros/noetic' != '' else []
